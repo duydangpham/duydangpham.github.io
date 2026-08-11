6c2882431208585b9e4841d378f53cc55b920338
duydangpham <dtdangpham@gmail.com>
2026-08-12 00:28:32 +0700

    Fix homepage portrait rendering

--- src/utils/portrait.ts
+++ src/utils/portrait.ts
@@ -1,15 +1,2 @@
-import { existsSync } from 'node:fs';
-import { fileURLToPath } from 'node:url';
-
-const portraitCandidates = [
-  '/assets/img/duydangpham_2026.JPG',
-  '/assets/img/duydangpham_2026.jpg',
-];
-
-const portraitSrc = portraitCandidates.find((src) => {
-  const absolutePath = fileURLToPath(new URL(`../../public${src}`, import.meta.url));
-  return existsSync(absolutePath);
-});
-
-export const profilePortraitSrc = portraitSrc;
-export const hasProfilePortrait = typeof portraitSrc === 'string';
+export const profilePortraitSrc = '/assets/img/duydangpham_2026.JPG';
+export const hasProfilePortrait = true;
